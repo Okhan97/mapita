3bda08f4dfe0463daf9d3878f0163aa57290d861
Okhan97 <26289334+Okhan97@users.noreply.github.com>
2023-12-20 00:12:55 -0300

    regions painted in map

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Mapita
 
 This is a map that I'm doing for fun
+
+GeoJSON data obtained from [https://github.com/caracena/chile-geojson](https://github.com/caracena/chile-geojson)
--- src/app/Mapita.tsx
+++ src/app/Mapita.tsx
@@ -4,17 +4,37 @@ import { useEffect } from "react";
 import { Map } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./Mapita.css";
+import regionesJSON from "../data/regiones.json";
 
 const MAP_DIV_ID = "mapita";
 
 const Mapita = () => {
   useEffect(() => {
-    new Map({
+    const map = new Map({
       container: MAP_DIV_ID,
       style: "mapbox://styles/mapbox/streets-v12",
       center: [-71, -35],
       zoom: 3.5,
       accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+    });
+    map.on("load", () => {
+      for (let i = 0; i < regionesJSON.features.length; i++) {
+        const color = i % 2 ? "#0080ff" : "#ff8c00";
+        map.addSource(regionesJSON.features[i].properties.Region, {
+          type: "geojson",
+          //@ts-ignore string type error
+          data: regionesJSON.features[i],
+        }),
+          map.addLayer({
+            id: regionesJSON.features[i].properties.Region,
+            type: "fill",
+            source: regionesJSON.features[i].properties.Region, // reference the data source
+            paint: {
+              "fill-color": color, // blue color fill
+              "fill-opacity": 0.5,
+            },
+          });
+      }
     });
   });
 
